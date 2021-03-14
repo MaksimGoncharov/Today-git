db497ed6cfd24dd52a03a1369bb7d88997272e9e
Max Pups <78901911+MaksimGoncharov@users.noreply.github.com>
2021-03-14 13:09:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,88 @@
-let buttonSave =document.querySelector('.button1');
-buttonSave.addEventListener('click', createDiv);
+ 
+
+let elems = document.querySelector('.button_one')
+elems.addEventListener('click', ButtonOne) 
+
+    function ButtonOne(){
+        alert(1)
+        elems.classList.add('none_but');  
+    }
+	
+let thing = document.querySelector('.button_two')
+    thing.addEventListener('click', ButTwo) 
+        function ButTwo(){
+            alert(2)
+            thing.classList.add('none_but');  
+        }	
+
+let allien = document.querySelector('.button_three')
+    allien.addEventListener('click', ButThr) 
+        function ButThr(){
+            alert(3)
+            allien.classList.add('none_but');  
+        }	
+let joe = document.querySelector('.button_four')
+    joe.addEventListener('click', ButFr) 
+        function ButFr(){
+            alert(4)
+            joe.classList.add('none_but');  
+        }	
+
+let elvis = document.querySelector('.button_five')
+    elvis.addEventListener('click', ButFV) 
+        function ButFV(){
+            alert(5)
+            elvis.classList.add('none_but');  
+        }	
+let Lenon = document.querySelector('.button_six')
+        Lenon.addEventListener('click', ButS) 
+            function ButS(){
+                alert(6)
+                Lenon.classList.add('none_but');  
+            }	
+let Jack = document.querySelector('.button_seven')
+            Jack.addEventListener('click', ButSev) 
+                function ButSev(){
+                    alert(7)
+                    Jack.classList.add('none_but');  
+                }	           
+let Tsoy = document.querySelector('.button_eight')
+                Tsoy.addEventListener('click', ButEi) 
+                    function ButEi(){
+                        alert(8)
+                        Tsoy.classList.add('none_but');  
+                    }	  
+let mike = document.querySelector('.button_nine')
+                    mike.addEventListener('click', ButNi) 
+                        function ButNi(){
+                            alert(9)
+                            mike.classList.add('none_but');  
+                        }	 
+let all = document.querySelector('.button_ten')
+                        all.addEventListener('click', ButTen) 
+                            function ButTen(){
+                                alert(10)
+                                all.classList.add('none_but');  
+                            }	
 
 
-function createDiv() {
-let cont=document.querySelector('.add_task');
-let task = document.querySelector('.input_task').value;
-let newDiv= document.createElement('div');
-let newText= document.createElement('textarea')
-newDiv.className='new_div';
-newText.innerHTML=task;
-cont.append(newDiv);
-newDiv.append(newText);
+                            
+                            
 
 
 
-let deleteTask = document.createElement('div');
-deleteTask.className='delete_task';
-deleteTask.innerHTML="❎"
-newDiv.append(deleteTask);
-
-
-}
-document.querySelector('body').addEventListener('click', onTask);
-
-function onTask(e){
-if(e.target.classList.contains('delete_task')) {
-    e.target.parentNode.remove()
-}
-}
 
 
 
-// калькулятор
 
 
-let value=0
-if (localStorage.getItem('value')) {
-    value=parseInt(localStorage.getItem('value'))
-    updateInput()
-}
-document
-       .querySelector('.min')
-       .addEventListener('click', () =>{
-          console.log('minus fired')
-          value--
-          updateInput()
-});
 
 
-document
-       .querySelector('.plus')
-       .addEventListener('click', () =>{
-          console.log('plus fired')
-          value++
-          updateInput()
-});
-function updateInput(){
-    document.querySelector('.inp').value = value
-    localStorage.setItem('value', value)
-}
+
+
+
+
+
 
 
 
